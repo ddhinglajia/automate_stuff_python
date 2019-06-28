90c22c767ca863d864e46b13ec83a4eb1380ae54
ddhinglajia <ddhinglajia@gmail.com>
2019-06-28 09:00:04 +0200

    typo update

--- 001_hello.py
+++ 001_hello.py
@@ -7,6 +7,7 @@ print('It is go to know you, ' + myName)
 #print('The length of your name is :' )
 #print(len(myName))
 print('The length of your name is : ' + str(len(myName)))
-print('Wat is your age?') #ask for their age
+print('What is your age?') #ask for their age
 myAge = input()
 print('You will be ' +str(int(myAge)+1) + ' in a year')
+d
